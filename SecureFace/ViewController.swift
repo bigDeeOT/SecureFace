4b6ac5fc7368b89862ef6a5acfe820f1c6e715d1
bigDeeOT <bigDee2008@gmail.com>
2018-07-31 20:32:41 -0700

    First Commit

---
+++ SecureFace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecureFace
+//
+//  Created by Dewayne Perry on 7/31/18.
+//  Copyright © 2018 The United States of America. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
